abd5cb758e24582c2d85aff0d1a9dea5ec882fd2
Anton-Latukha <anton.latukha@gmail.com>
2018-08-16 20:01:52 +0300

    rm profile messages

--- scripts/install-nix-from-closure.sh
+++ scripts/install-nix-from-closure.sh
@@ -25,24 +25,3 @@
 
 
 
-if [ -z "$added" ]; then
-    cat >&2 <<EOF
-
-Installation finished!  To ensure that the necessary environment
-variables are set, please add the line
-
-  . $p
-
-to your shell profile (e.g. ~/.profile).
-EOF
-else
-    cat >&2 <<EOF
-
-Installation finished!  To ensure that the necessary environment
-variables are set, either log in again, or type
-
-  . $p
-
-in your shell.
-EOF
-fi
