b6e12aeeaeb943b24929091ab555bd3a750fcd89
Anton-Latukha <anton.latukha@gmail.com>
2018-08-16 20:04:06 +0300

    add note for #!/bin/sh

--- scripts/install-nix-from-closure.sh
+++ scripts/install-nix-from-closure.sh
@@ -1,1 +1,5 @@
 #!/bin/sh
+## NOTE: If you would decide to change shebang to `/usr/bin/env sh`
+## - investigate the question more to be really sure,
+## as `/usr/bin/env sh` usage is less portable then `/bin/sh`,
+## at least - using env is much less secure.
